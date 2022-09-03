034d8e14ea58a75daf0639b7ad6d158e35d42ff3
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-09-03 15:00:14 +0300

    First programm commit

---
+++ programm_001_second_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа, которая принимает на вход случайно сгенерированное трёхзначное число и на выходе показывает вторую цифру этого числа.
+Random rnd = new Random();
+
+int Number = rnd.Next(100,1000);
+
+Console.WriteLine($"Случайное число - {Number}");
+
+int first_digit = Number / 100;
+
+
+int second_digit = (Number - first_digit * 100) / 10;
+Console.WriteLine($"Вторая цифра случайного числа - {second_digit}");
+
